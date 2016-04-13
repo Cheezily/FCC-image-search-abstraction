6a509550ba4447cd3086249161deb69f5d1561ce
cheezily <pm655@yahoo.com>
2016-04-13 18:05:42 -0500

    add ability to offset results as url query parameter

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 var http = require('http');
+var url = require('url');
+var request = require('request');
 
-var API_KEY = process.env.API_KEY || "my key";
+var API_KEY = process.env.API_KEY || require('./apikey').API_KEY();
 var bing = require('node-bing-api')({ accKey: API_KEY });
 
 var server = http.createServer(function(req, res) {
 
-  var searchTerm = req.url.substring(1);
+  //the search term needs to be captured with url pathname now that queries
+  //are being accepted
+  console.log("Search Term: " + url.parse(req.url).pathname.substring(1));
+  var searchTerm = url.parse(req.url).pathname.substring(1);
+  var query = url.parse(req.url, true).query;
+
+  //get the number of results to skip as a query parameter
+  var offset = 0;
+  if (query['offset']) {
+    offset = parseInt(query['offset']);
+  }
 
   bing.images(searchTerm, {
-    top: 10
+    top: 10 + offset,
+    skip: offset,
   },
   function(err, response, body) {
     sendOut(body['d']['results']);
@@ -19,7 +32,7 @@ var server = http.createServer(function(req, res) {
 
     var output = [];
 
-    console.log(responses.length);
+    //console.log(responses.length);
     for (var i = 0; i < responses.length; i++) {
       var item = {};
       item.url = responses[i]['MediaUrl'];
